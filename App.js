92e0179f0fe7b493864e5d89933ef6a1445577cc
Jerem <jeremy.agathon@laposte.net>
2024-05-04 12:59:40 +0200

    push to data throught parent to child and child to parent

--- App.js
+++ App.js
@@ -2,38 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import Product from './components/Products';
+import AddProduct from './components/AddProducts';
 
 export default function App() {
 
   const [tabArticles,setTabArticles] = useState([])
-  const [articleName,setArticleName] = useState("")
 
-  const inputHandler = (name ) =>{
-    setArticleName(name)
-  }
 
-  const submitHandler = () =>{
+  const submitHandler = (articleName) =>{
     const idString = Date.now().toString();
-
-    
     setTabArticles(currentArticles => [{key: idString, name : articleName},...currentArticles])
-    setArticleName('') //reset le champs de l'article actuel 
+   // setArticleName('') //reset le champs de l'article actuel 
     console.log(tabArticles)
   }
 
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput style = {styles.textInput}
-        placeholder='Saisir votre acticle'
-        onChangeText={inputHandler}
-        value = {articleName}
-        >
-        </TextInput>
-        
-        <Button title="Enregistrer l'article" onPress={submitHandler}>
-        </Button>
-      </View>
+      <AddProduct submitHandler = {submitHandler}/>
         <FlatList
         data = {tabArticles}
         renderItem={({item}) => <Product name = {item.name}/> }>
@@ -49,14 +34,6 @@ const styles = StyleSheet.create({
   padding:40,
   paddingTop:60
   },
-  inputContainer:{
-
-  },
-  textInput:{
-    borderColor:'grey',
-    padding :5,
-    paddingLeft :10,
-    flexGrow:1
-  },
+  
   
 });
---
+++ components/AddProducts.js
@@ -0,0 +1,42 @@
+import React,{useState} from "react";
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+
+const AddProduct = ({submitHandler}) =>{
+
+    const [articleName,setArticleName] = useState("")
+    const inputHandler = (name ) =>{
+        setArticleName(name)
+    }
+
+    return(
+        <View style={styles.inputContainer}>
+             <TextInput style = {styles.textInput}
+                placeholder='Saisir votre acticle'
+                onChangeText={inputHandler}
+                value = {articleName}>
+            </TextInput>
+        
+        <Button title="Enregistrer l'article" onPress={() => submitHandler(articleName)}>
+        </Button>
+
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        flexDirection:"row",
+        marginBottom: 15
+
+    },
+    textInput:{
+      borderColor:'grey',
+      padding :5,
+      paddingLeft :10,
+      flexGrow:1,
+      fontSize:18
+    },
+})
+
+export default AddProduct
