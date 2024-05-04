6997dbc441959c43c058bd5051e63cca25938a84
Jerem <jeremy.agathon@laposte.net>
2024-05-04 11:25:48 +0200

    Init project

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [tabArticles,setTabArticles] = useState([])
+  const [articleName,setArticleName] = useState("")
+
+  const inputHandler = (name ) =>{
+    setArticleName(name)
+  }
+
+  const submitHandler = () =>{
+    setTabArticles(currentArticles => [...currentArticles,articleName])
+    setArticleName('') //reset le champs de l'article actuel 
+    console.log(tabArticles)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput style = {styles.textInput}
+        placeholder='Saisir votre acticle'
+        onChangeText={inputHandler}
+        value = {articleName}
+        >
+        </TextInput>
+        
+        <Button title="Enregistrer l'article" onPress={submitHandler}>
+        </Button>
+      </View>
+      <ScrollView>
+      <View style={styles.item}>
+        {
+          tabArticles.map((articleName,index) => (
+          <Text style ={styles.element} key={index} > {articleName} </Text>
+          ))
+        }
+
+      </View>
+      </ScrollView>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  padding:40,
+  paddingTop:60
   },
+  inputContainer:{
+
+  },
+  textInput:{
+    borderColor:'grey',
+    padding :5,
+    paddingLeft :10,
+    flexGrow:1
+  },
+  item:{
+    //backgroundColor : "red"
+    marginTop : 10
+  },
+  element:{
+    backgroundColor : "yellow",
+    padding : 20,
+    fontSize : 17, // police du text
+    marginVertical : 6 //permet de definir des marges vertical plus facilement 
+    //qu'avec margin top et botom
+  }
 });
