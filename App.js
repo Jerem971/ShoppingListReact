784a575197fcc7aab861fdb0364c2ff99bf0ba83
Jerem <jeremy.agathon@laposte.net>
2024-05-06 00:18:30 +0200

    Alert

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList, Alert} from 'react-native';
 import Product from './components/Products';
 import AddProduct from './components/AddProducts';
 
@@ -11,9 +11,33 @@ export default function App() {
 
   const submitHandler = (articleName) => {
     const idString = Date.now().toString();
-    setTabArticles(currentArticles => [{key: idString, name : articleName},...currentArticles])
-   // setArticleName('') //reset le champs de l'article actuel 
-    console.log(tabArticles)
+    if(articleName.length > 1){
+      setTabArticles(currentArticles => [{key: idString, name : articleName},...currentArticles])
+    // setArticleName('') //reset le champs de l'article actuel 
+      console.log(tabArticles)
+    }else{
+      //alert('refusé')
+      Alert.alert('Désole','le nombre de char doit etre supérieur à 1',[
+        {
+          text : 'Compris',
+          onPress: () => console.log('compris clicked')
+        },
+        {
+          text : 'OK',
+          onPress: () => console.log('OK clicked')
+        },
+        {
+          text : 'Yes',
+          onPress: () => console.log('yes clicked')
+        },
+      
+      ],
+      {
+        cancelable : 'true',
+        onDismiss : () => console.log('dismiss')
+      }
+      )
+    }
   }
 
   const deleteArticle = (key) => {
